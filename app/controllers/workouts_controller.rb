afd7df07cbc4d1fb74f075f9b7c00701aa95194c
Steve Pentler <stevepentler@gmail.com>
2016-02-18 13:26:21 -0700

    Refactors Faraday methods, headers now part of intialize do block

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,8 +2,8 @@ require 'will_paginate/array'
 
 class WorkoutsController < ApplicationController
   def index
-    @methods = WorkoutService.new(current_user)
-    @workouts = @methods.workouts
+    @methods = WorkoutService.new
+    @workouts = @methods.workouts_for(current_user)
                         .paginate(page: params[:page], per_page: 10)
   end
 
