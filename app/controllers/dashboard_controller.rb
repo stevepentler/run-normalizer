f99fb015d0ef52da85cd1f64ce7c4782ce5382a5
Steve Pentler <stevepentler@gmail.com>
2016-02-19 14:33:05 -0700

    Refactors dashboard controller to make one API call

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,6 @@ class DashboardController < ApplicationController
   
   def show
     @methods = AggregateDataService.new(current_user)
-    @aggregate_data = AggregateDataService.new(current_user).aggregate_data_for(current_user)
+    @aggregate_data = @methods.aggregate_data_for(current_user)
   end
 end
--- test/services/workout_service_test.rb
+++ test/services/workout_service_test.rb
@@ -2,7 +2,7 @@ class WorkoutServiceTest <ActiveSupport::TestCase
   test "#workouts" do
     VCR.use_cassette('workouts') do
       steve = users(:steve)
-      methods = WorkoutService.new
+      methods = WorkoutService.new(steve)
       workout = methods.workouts_for(steve).first
       assert_equal "2012-08-06T16:25:04+00:00", workout["created_datetime"]
       assert_equal "8:13", methods.average_pace(workout)
