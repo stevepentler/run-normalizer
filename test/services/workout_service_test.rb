3605d4394f62005e3a1496c2dfe827555b7ca83e
Steve Pentler <stevepentler@gmail.com>
2016-02-18 22:16:27 -0700

    Adds backgrond image and spacing to dashboard, adds commas to lifetime statistics, exports lifetime stats to partial

--- test/services/workout_service_test.rb
+++ test/services/workout_service_test.rb
@@ -4,7 +4,7 @@ class WorkoutServiceTest <ActiveSupport::TestCase
       steve = users(:steve)
       methods = WorkoutService.new
       workout = methods.workouts_for(steve).first
-      
+
       assert_equal "2012-08-06T16:25:04+00:00", workout["created_datetime"]
       assert_equal "8:13", methods.average_pace(workout)
       assert_equal 13.01, methods.distance(workout)
