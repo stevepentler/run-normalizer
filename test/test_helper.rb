920297d4586f994f0edabf2625fb7997fad6f1a1
Steve Pentler <stevepentler@gmail.com>
2016-02-18 18:49:41 -0700

    Refactors oauth hash

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,20 +23,29 @@ class ActiveSupport::TestCase
   OmniAuth.config.test_mode = true
   omniauth_hash = 
 {"provider"=>"mapmyfitness",
+ "uid"=>nil,
  "info"=>
-  {"weight"=>78.01788764,
+  {"last_name"=>"pentler",
+   "weight"=>78.01788764,
    "height"=>1.8542,
+   "hobbies"=>"",
    "id"=>8695588,
+   "date_joined"=>"2011-08-08T21:24:11+00:00",
+   "first_name"=>"steven",
    "display_name"=>"steven pentler",
+   "introduction"=>"",
+   "display_measurement_system"=>"imperial",
+   "last_login"=>"2014-02-18T16:14:33+00:00",
    "location"=>{"country"=>"US", "region"=>"WI", "locality"=>"Brookfield", "address"=>""},
    "email"=>"stevepentler@gmail.com",
    "username"=>"pentler",
+   "sharing"=>{"twitter"=>false, "facebook"=>false},
    "gender"=>"M",
+   "time_zone"=>"America/Denver",
    "birthdate"=>"1991-05-23",
- "credentials"=>{"token"=>"258be676eee8b7b05488cc158b25ad2e6cdfd46d", "refresh_token"=>"2bab757e1a9fec3586161df37cdd8cc866688737", "expires_at"=>1460520194, "expires"=>true}
-  }
-}
-
+   "profile_statement"=>""},
+ "credentials"=>{"token"=>"258be676eee8b7b05488cc158b25ad2e6cdfd46d", "refresh_token"=>"2bab757e1a9fec3586161df37cdd8cc866688737", "expires_at"=>1460520194, "expires"=>true},
+ "extra"=>{}}
 
   OmniAuth.config.add_mock(:mapmyfitness, omniauth_hash)
 end
