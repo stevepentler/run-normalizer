b78e74a06d29e0caf269ecad47a69af88ca8cea8
Steve Pentler <stevepentler@gmail.com>
2016-02-19 10:26:51 -0700

    Adds VCR to user_session_test

--- test/integration/user_sessions_test.rb
+++ test/integration/user_sessions_test.rb
@@ -6,64 +6,72 @@ class UserSessionsTest < ActionDispatch::IntegrationTest
   end
 
   test "Succesful User Login" do
-    user = users(:steve)
-    
-    visit root_path
-    click_on "Login with Map My Fitness"
-    
-    visit dashboard_path
-    assert page.has_content?(user.username)
-    assert page.has_content?("View Workouts")
-    assert page.has_content?("miles")
-    assert page.has_content?("calories")
-    assert page.has_content?("steps")
-    assert page.has_content?("sessions")
+    VCR.use_cassette('user') do
+      user = users(:steve)
+      
+      visit root_path
+      click_on "Login with Map My Fitness"
+      
+      visit dashboard_path
+      assert page.has_content?(user.username)
+      assert page.has_content?("View Workouts")
+      assert page.has_content?("miles")
+      assert page.has_content?("calories")
+      assert page.has_content?("steps")
+      assert page.has_content?("sessions")
 
-    click_on "View Workouts"
-    assert_equal workouts_path, current_path
+      click_on "View Workouts"
+      assert_equal workouts_path, current_path
 
-    assert page.has_content?(user.username)
-    assert page.has_content?("workouts")
-    assert page.has_content?("Date")
-    assert page.has_content?("Started At")
-    assert page.has_content?("Distance")
-    assert page.has_content?("Duration")
-    assert page.has_content?("Avg-Speed")
-    assert page.has_content?("Avg-Pace")
-    assert page.has_content?("Calories")
+      assert page.has_content?(user.username)
+      assert page.has_content?("workouts")
+      assert page.has_content?("Date")
+      assert page.has_content?("Started At")
+      assert page.has_content?("Distance")
+      assert page.has_content?("Duration")
+      assert page.has_content?("Avg-Speed")
+      assert page.has_content?("Avg-Pace")
+      assert page.has_content?("Calories")
+    end
   end
 
   test "Succesful User Logout" do
     login
-    user = users(:steve)
-    
-    visit root_path
-    click_on "Login with Map My Fitness"
-    
-    visit dashboard_path
-    assert page.has_content?(user.username)
+    VCR.use_cassette('user') do
+      user = users(:steve)
+      
+      visit root_path
+      click_on "Login with Map My Fitness"
+      
+      visit dashboard_path
+      assert page.has_content?(user.username)
 
-    click_on "View Workouts"
-    assert_equal workouts_path, current_path
-    assert page.has_content?(user.username)
+      click_on "View Workouts"
+      assert_equal workouts_path, current_path
+      assert page.has_content?(user.username)
 
-    click_on "Dashboard"
-    assert_equal dashboard_path, current_path
+      click_on "Dashboard"
+      assert_equal dashboard_path, current_path
 
-    click_on "Logout"
-    assert_equal root_path, current_path
+      click_on "Logout"
+      assert_equal root_path, current_path
+    end
   end
 
   test "Unregistered User Attempts to View Dashboard" do
-    visit dashboard_path
-    assert_equal root_path, current_path
-    assert page.has_content?("Please login to access your data")
+    VCR.use_cassette('user') do
+      visit dashboard_path
+      assert_equal root_path, current_path
+      assert page.has_content?("Please login to access your data")
+    end
   end
 
   test "Unregistered User Attempts to View Workouts" do
-    visit workouts_path
-    assert_equal root_path, current_path
-    assert page.has_content?("Please login to access your data")
+    VCR.use_cassette('user') do
+      visit workouts_path
+      assert_equal root_path, current_path
+      assert page.has_content?("Please login to access your data")
+    end
   end
 
 end
